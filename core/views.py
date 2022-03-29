f285fdff31bba9177048210e8afca3f0ebea8042
Ridwan Salmanu <ridwan.salmanu96@yahoo.com>
2022-03-29 21:48:16 +0100

    templates functioning nav's

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Item
 
-# Create your views here.
+
+def home(request):
+    context = {}
+    return render(request, 'home.html', context)
+
+def checkout(request):
+    context = {}
+    return render(request, 'checkout.html', context)
+
+def product(request):
+    context = {}
+    return render(request, 'product.html', context)
